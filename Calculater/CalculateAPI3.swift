2546b69f8514d20ec9001da38f322d862ee6c927
Terry Liou <terry@gamer.com.tw>
2017-06-30 14:49:12 +0800

    CalculaterAPI3 完成

--- Calculater/CalculateAPI3.swift
+++ Calculater/CalculateAPI3.swift
@@ -39,8 +39,6 @@ struct CalculateBrind3 {
         prepareStringFormula.formulaDescription[arrayIndex] = (prepareStringFormula.resultIsPending) ? String(format: "%g", digit): String(format: "%g", digit)
         stringForLabelDisplay = prepareStringFormula.formulaDescription.joined()
         isNewDigit = true
-        prepareStringFormula.resultIsPending = true
-//        stringForLabelDisplay = (resultIsPending) ? stringForLabelDisplay + String(format: "%g", digit) : String(format: "%g", digit)
     }
     
     private var prepareToOperate: PrepareToOperate?
@@ -56,7 +54,7 @@ struct CalculateBrind3 {
 
     var prepareStringFormula = PrepareStringFormula()
 
-    struct PrepareStringFormula {
+    struct PrepareStringFormula {                                                           // 處理字串顯示的 struct
         var tmpOperand = ""
         var resultIsPending = false
         var isAdditionOrSubtractionAtFirst = false
@@ -64,30 +62,37 @@ struct CalculateBrind3 {
         var haveParentheses = false
         var formulaDescription = Array.init(repeating: "", count: 3)
 
-        mutating func unaryFormulaCombine(by sign: String, with digit: String) -> String {
+        mutating func unaryFormulaCombine(by sign: String) -> String {                      // 處理一元運算子的 func
             let arrayIndex = (resultIsPending) ? 2: 0
-            tmpOperand = (haveParentheses) ? tmpOperand: formulaDescription[0]
-            if sign == "-" && haveParentheses {
-                formulaDescription[arrayIndex] = (resultIsPending) ? digit: tmpOperand
+            tmpOperand = (haveParentheses) ? tmpOperand: formulaDescription[arrayIndex]     // 趁運算數未被括號處理前存起來
+
+            if sign == "-" && haveParentheses {              // 當字串已經有括號，表示第二次按需要去括號
+                formulaDescription[arrayIndex] = tmpOperand
                 haveParentheses = false
-            } else if sign == "-" && !haveParentheses {
-                formulaDescription[arrayIndex] = (resultIsPending) ? "(-(\(digit)))": "-(\(formulaDescription[0]))"
+            } else if sign == "-" && !haveParentheses {      // 變號鍵的特性第一次按一定要加上括號
+                formulaDescription[arrayIndex] = (resultIsPending) ? "(-(\(tmpOperand)))": "-(\(tmpOperand))"
                 haveParentheses = true
             } else {
-                formulaDescription[arrayIndex] = (resultIsPending) ? "\(sign)(\(digit))": "\(sign)(\(formulaDescription[0]))"
+                tmpOperand = formulaDescription[arrayIndex]  // 按下其他一元運算子表示正負號已經確定，將暫存更新
+                formulaDescription[arrayIndex] = "\(sign)(\(formulaDescription[arrayIndex]))"
+                haveParentheses = false
             }
+
             return formulaDescription.joined()
         }
 
         mutating func binaryFormulaCombine(by sign: String) -> String {
+            let isContainParentheses = formulaDescription[0].characters.last == ")" && formulaDescription[0].characters.first == "(" // 判斷字串是否含有括號
             isMultiplyOrDividedAtSecend = (sign == "×" || sign == "÷") ? true: false
-            haveParentheses = isMultiplyOrDividedAtSecend && isAdditionOrSubtractionAtFirst
+            haveParentheses = isMultiplyOrDividedAtSecend && isAdditionOrSubtractionAtFirst  // 判斷先乘除後加減
             formulaDescription[1] = sign
+
             if haveParentheses {
-                formulaDescription[0] = (formulaDescription[0].characters.last == ")") ? formulaDescription[0]: "(\(formulaDescription[0]))"
+                formulaDescription[0] = (isContainParentheses) ? formulaDescription[0]: "(\(formulaDescription[0]))"
             } else {
-                formulaDescription[0] = (formulaDescription[0].characters.last == ")") ? String(formulaDescription[0].characters.dropLast(1).dropFirst(1)): formulaDescription[0]
+                formulaDescription[0] = (isContainParentheses) ? String(formulaDescription[0].characters.dropLast(1).dropFirst(1)): formulaDescription[0]
             }
+
             return formulaDescription.joined()
         }
 
@@ -96,104 +101,50 @@ struct CalculateBrind3 {
             formulaDescription[1] = ""
             formulaDescription[2] = ""
             isAdditionOrSubtractionAtFirst = !isMultiplyOrDividedAtSecend
-        }
+        }   // 在確認計算後，從前一次 isMultiplyOrDividedAtSecend 的判斷來確定 isAdditionOrSubtractionAtFirst的值
     }
     
     mutating func preformOperation(by sign: String) {
+
         if let symbol = operatedSign[sign] {
-            
             switch symbol {
-                
             case .constant(let digit):
                 displayDigit = digit
                 let arrayIndex = (prepareStringFormula.resultIsPending) ? 2: 0
                 prepareStringFormula.formulaDescription[arrayIndex] = (prepareStringFormula.resultIsPending) ? sign: sign
                 stringForLabelDisplay = prepareStringFormula.formulaDescription.joined()
-//                stringForLabelDisplay = (resultIsPending) ? stringForLabelDisplay + sign: sign
-                prepareStringFormula.resultIsPending = true
+                isNewDigit = true
                 
             case .unaryOperator(let function):
-                
-                if let digit = displayDigit {
-                    let tmpSign = (sign == "±") ? "-" : sign
-                    displayDigit = function(digit)
-//                    if prepareStringFormula.resultIsPending {
-//                        stringForLabelDisplay = String(stringForLabelDisplay.characters.dropLast(String(format: "%g", digit).characters.count)) + "\(tmpSign)(\(String(format: "%g", digit)))"
-//                    } else {
-//                        stringForLabelDisplay = "\(tmpSign)(\(stringForLabelDisplay))"
-//                    }
-                    stringForLabelDisplay = prepareStringFormula.unaryFormulaCombine(by: tmpSign,
-                                                                                     with: String(format: "%g", digit))
-//                    prepareStringFormula.resultIsPending = false
+
+                if isNewDigit {
+                    if let digit = displayDigit {
+                        let tmpSign = (sign == "±") ? "-" : sign
+                        displayDigit = function(digit)
+                        stringForLabelDisplay = prepareStringFormula.unaryFormulaCombine(by: tmpSign)
+                    }
                 }
                 
             case .binaryOperator(let function):
-                
-                if let digit = displayDigit {
-//                    if stringForLabelDisplay != "" {
-//                        if prepareToOperate == nil {
-//                            prepareToOperate = PrepareToOperate(firstOperand: digit, function: function)
-//                            prepareStringFormula.resultIsPending = true
-//                        }else{
-//                            displayDigit = prepareToOperate?.execute(with: displayDigit!)
-//                            prepareToOperate = PrepareToOperate(firstOperand: displayDigit!, function: function)
-//                        }
-//
-//                        if prepareStringFormula.resultIsPending {
-//                            stringForLabelDisplay += sign
-//                        }
-//                    }
 
+                if let digit = displayDigit {
+                    prepareStringFormula.resultIsPending = true
                     if isNewDigit {                     // 表示按運算符號前有輸入數字
                         if prepareToOperate == nil {    // 還沒有公式的情況
-                            prepareToOperate = PrepareToOperate(firstOperand: digit, function: function)
-                            stringForLabelDisplay = prepareStringFormula.binaryFormulaCombine(by: sign)
+                            prepareToOperate = PrepareToOperate(firstOperand: digit, function: function)        // 產生輸入數字和算符的公式
                         } else {
-                            displayDigit = prepareToOperate?.execute(with: digit)
-                            prepareToOperate = PrepareToOperate(firstOperand: displayDigit!, function: function)
-                            prepareStringFormula.commitFormula()
-                            stringForLabelDisplay = prepareStringFormula.binaryFormulaCombine(by: sign)
+                            displayDigit = prepareToOperate?.execute(with: digit)                               // 先前產生的公式實行運算
+                            prepareToOperate = PrepareToOperate(firstOperand: displayDigit!, function: function)// 產生運算結果和算符的公式
+                            prepareStringFormula.commitFormula()                                                // 計算完後將字串組合
                         }
                         isNewDigit = false
                     } else if stringForLabelDisplay != "0" {
-                        prepareToOperate = PrepareToOperate(firstOperand: displayDigit!, function: function)
-                        stringForLabelDisplay = prepareStringFormula.binaryFormulaCombine(by: sign)
+                        prepareToOperate = PrepareToOperate(firstOperand: displayDigit!, function: function)    // 重複按下二元運算子時重新產生公式
                     }
+                    stringForLabelDisplay = prepareStringFormula.binaryFormulaCombine(by: sign)                 // 秀出公式
                     prepareStringFormula.isMultiplyOrDividedAtSecend = (sign == "×" || sign == "÷") ? true: false
                 }
-//
-//                private mutating func executeFomula(by digit: Double) {
-//                    displayDigit = prepareToOperate?.execute(with: digit)
-//                    displayFormula.commit(wiht: modifyingOperater + modifyingOperand, haveParentheses: haveParentheses)
-//                    frontOperattionIsAdditionOrSubtraction = !secnedOperattionIsMultiplyOrDivided
-//                }
 
-//                displayFormula.resultIsPending = true
-//                if let digit = tmpOperand {
-//                    //==============================判斷公式存在於否的計算邏輯============================
-//                    if modifyingOperand == "" {
-//                        modifyingOperand = " \(modifyDouble(digit))"
-//                    }
-//
-//                    if prepareToOperate != nil {
-//                        executeFomula(by: digit)
-//                        prepareToOperate = PrepareToOperate(firstOperand: displayDigit!, function: function)
-//                    } else {
-//                        prepareToOperate = PrepareToOperate(firstOperand: digit, function: function)
-//                        displayFormula.commit(wiht: modifyingOperand, haveParentheses: false)
-//                    }
-//                    modifyingOperater = " \(sign)"
-//                    tmpOperand = nil
-//
-//                } else if stringForLabelDisplay != "0" {
-//                    modifyingOperater = " \(sign)"
-//                    prepareToOperate = PrepareToOperate(firstOperand: displayDigit!, function: function)
-//                }
-//                // =======================產生顯示公式字串的邏輯=====================================
-//
-//                secnedOperattionIsMultiplyOrDivided = (modifyingOperater == " ×" || modifyingOperater == " ÷") ? true: false
-//                stringForLabelDisplay = displayFormula.displayFormulaSubmit(modifyingOperater,haveParentheses: haveParentheses)
-//                modifyingOperand = ""
             case .equal:
                 
                 if prepareToOperate != nil && displayDigit != nil {
@@ -205,7 +156,6 @@ struct CalculateBrind3 {
             }
         }
     }
-
     var result: Double? {
         
         get {
